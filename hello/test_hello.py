f9551d83eac2f7a8aa2b348cc2e1b1ab2e45ec42
cholo <waleyolokunola@gmail.com>
2025-11-19 14:23:31 +0100

    change text in the hello_test.py

--- hello/test_hello.py
+++ hello/test_hello.py
@@ -4,4 +4,4 @@ class HelloTests(TestCase):
     def test_index_view(self):
         response = self.client.get('/')
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Hello CI/CD!")
+        self.assertContains(response, "Hello coloooeojeojejejeCI/CD!")
