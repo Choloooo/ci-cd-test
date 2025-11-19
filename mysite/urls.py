1471feea004fa6585ad752e51cc9709754c7619b
cholo <waleyolokunola@gmail.com>
2025-11-19 12:05:08 +0100

    Add root URL and index view for CI/CD test

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+# mysite/urls.py
 from django.contrib import admin
 from django.urls import path
+from hello.views import index  # import your view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),  # root URL points to index view
 ]
+
